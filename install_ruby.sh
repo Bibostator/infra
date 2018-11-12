2e7757ff119158a83eb7903997289cc0e7885561
Alex Korbut <bibostator@gmail.com>
2018-11-13 01:44:36 +0300

    Changed Ruby install

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+unset rvm_path
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 rvm requirements
