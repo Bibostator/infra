08ecd327be02f9c0f3f17ce3db9a3130ee6ec552
Alex Korbut <bibostator@gmail.com>
2018-11-12 21:43:01 +0300

    Changed deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 #installing Ruby
-chmod u+x ~/install_ruby.sh
-~/install_ruby.sh
+#chmod u+x ~/install_ruby.sh
+#~/install_ruby.sh
 #installing MongoDB
-chmod u+x ~/install_mongodb.sh
-~/install_mongodb.sh
+#chmod u+x ~/install_mongodb.sh
+#/install_mongodb.sh
+#NO NEEDS TO DEPLOY STUFF ABOVE
 # Copying data from GIT
 git clone https://github.com/Artemmkin/reddit.git
 # Install dependences
