c013935c5db6d9c6fa1c7d00464a6297906d6d6d
Alex Korbut <bibostator@gmail.com>
2018-11-13 02:48:48 +0300

    Changed

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 wget -O - 'https://raw.githubusercontent.com/Bibostator/infra/master/install_ruby.sh' | bash
 wget -O - 'https://raw.githubusercontent.com/Bibostator/infra/master/install_mongodb.sh' | bash
-source ~/.rvm/scripts/rvm
 wget -O - 'https://raw.githubusercontent.com/Bibostator/infra/master/deploy.sh' | bash
