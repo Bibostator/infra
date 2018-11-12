5da7dbb7a1c51dd33ce32e63b72dda204c125bf8
Alex Korbut <bibostator@gmail.com>
2018-11-13 01:29:36 +0300

    Changed links3

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-wget -qcO 'https://raw.githubusercontent.com/Bibostator/infra/master/install_ruby.sh' && sh ~/install_ruby.sh
-wget -qcO 'https://raw.githubusercontent.com/Bibostator/infra/master/install_mongodb.sh' && sh ~/install_mongodb.sh
-wget -qcO 'https://raw.githubusercontent.com/Bibostator/infra/master/deploy.sh' && sh ~/deploy.sh
+wget -O - 'https://raw.githubusercontent.com/Bibostator/infra/master/install_ruby.sh' | bash
+wget -O - 'https://raw.githubusercontent.com/Bibostator/infra/master/install_mongodb.sh' | bash
+wget -O - 'https://raw.githubusercontent.com/Bibostator/infra/master/deploy.sh' | bash
